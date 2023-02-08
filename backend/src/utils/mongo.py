74ba7b43d68922c5fe5931a06f7be58038b8228f
Aaron Targa <aaron.targa@gmail.com>
2023-02-08 15:36:07 +0100

    chore(docker): add mongodb to docker configuration

--- backend/src/utils/mongo.py
+++ backend/src/utils/mongo.py
@@ -1,9 +1,14 @@
 from pymongo import MongoClient
+import os
 
 class Mongo():
 
     def __init__(self):
-        self.client = MongoClient('mongodb://localhost:27017/')
+        host = os.getenv("DB_HOST")
+        user = os.getenv("MONGO_INITDB_ROOT_USERNAME")
+        password = os.getenv("MONGO_INITDB_ROOT_PASSWORD")
+        self.client = MongoClient(f'mongodb://{user}:{password}@{host}:27017/')
+        print(f'mongodb://{user}:{password}@{host}:27017/')
         self.db = self.client['ctrleth']
 
     def close(self):
