19c1bc46f000a1ef4b157898d02b67bf4bf98ae3
Aaron Targa <aaron.targa@gmail.com>
2023-03-17 11:14:52 +0100

    feat(frontend-settings): categorizing settings and adding tooltips

--- webclient/src/Components/Dialogs/SettingsDialog.tsx
+++ webclient/src/Components/Dialogs/SettingsDialog.tsx
@@ -1,12 +1,14 @@
 import DialogTitle from '@mui/material/DialogTitle';
 import Dialog from '@mui/material/Dialog';
 import Button from '@mui/material/Button';
+import InfoIcon from '@mui/icons-material/Info';
 import { SettingsContext } from '../../Context';
-import { DialogActions, DialogContent, TextField } from '@mui/material';
+import { DialogActions, DialogContent, IconButton, Link, TextField, Tooltip, Typography } from '@mui/material';
 import { useContext, useState } from 'react';
 import { Settings } from '../../types/types';
 import Stack from '@mui/material/Stack';
 import { retrieveSettings } from '../../lib/settings';
+import styled from '@emotion/styled';
 
 export interface DialogProps {
   open: boolean;
@@ -14,62 +16,120 @@ export interface DialogProps {
   onSave: (settings: Settings) => void;
 }
 
-
-
+const StretchedTextfield = styled(TextField)`
+  flex-grow: 1;
+`;
+const InfoText = styled.p`
+  font-size: 1.5em;
+  text-align: center;
+`;
 
 export function SettingsDialog(props: DialogProps) {
   const { open, onClose, onSave } = props;
-  const { settings,setSettings } = useContext(SettingsContext);
+  const { settings, setSettings } = useContext(SettingsContext);
   const [localSettings, setLocalSettings] = useState(settings);
 
   const reset = () => {
-    localStorage.removeItem("settings");
+    localStorage.removeItem('settings');
     setSettings(retrieveSettings());
-    setLocalSettings(retrieveSettings())
-  }
+    setLocalSettings(retrieveSettings());
+  };
 
   return (
     <Dialog open={open} onClose={onClose}>
       <DialogTitle>Settings</DialogTitle>
-      <DialogContent>
+      <DialogContent sx={{ width: '400px' }}>
         <Stack spacing={2} sx={{ mt: '2em' }}>
-        <TextField
-            id="secret"
-            label="Create Secret"
-            type="text"
-            value={localSettings.secret}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(event) => {
-              setLocalSettings({ ...localSettings, secret: event.target.value });
-            }}
-          />
-          <TextField
-            id="etherscan"
-            label="Etherscan Token"
-            type="text"
-            value={localSettings.etherscan}
-            InputLabelProps={{
-              shrink: true,
-            }}
-            onChange={(event) => {
-              setLocalSettings({ ...localSettings, etherscan: event.target.value });
-            }}
-          />
-          <TextField
-            id="rpc"
-            label="RPC URL"
-            type="text"
-            InputLabelProps={{
-              shrink: true,
-            }}
-            value={localSettings.rpc}
-            onChange={(event) => {
-              setLocalSettings({ ...localSettings, rpc: event.target.value });
-            }}
-          />
-          <TextField
+          <Typography>
+           Credentials
+          </Typography>
+          <Stack direction="row">
+            <StretchedTextfield
+              id="secret"
+              label="Create Secret"
+              type="text"
+              value={localSettings.secret}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(event) => {
+                setLocalSettings({ ...localSettings, secret: event.target.value });
+              }}
+            />
+            <Tooltip title={<InfoText>Authentication Key required to be able to start own disassembly tasks</InfoText>}>
+              <IconButton>
+                <InfoIcon />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <Stack direction="row">
+            <StretchedTextfield
+              id="etherscan"
+              label="Etherscan Token"
+              type="text"
+              value={localSettings.etherscan}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              onChange={(event) => {
+                setLocalSettings({ ...localSettings, etherscan: event.target.value });
+              }}
+            />
+            <Tooltip
+              title={
+                <InfoText>
+                  Token required to use the{' '}
+                  <Link href="https://etherscan.io/apis" target="_blank" rel="noopener noreferrer">
+                    Etherscan API
+                  </Link>
+                </InfoText>
+              }
+            >
+              <IconButton>
+                <InfoIcon />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <Stack direction="row">
+            <StretchedTextfield
+              id="rpc"
+              label="RPC URL"
+              type="text"
+              InputLabelProps={{
+                shrink: true,
+              }}
+              value={localSettings.rpc}
+              onChange={(event) => {
+                setLocalSettings({ ...localSettings, rpc: event.target.value });
+              }}
+            />
+            <Tooltip
+              title={
+                <InfoText>
+                  <Link
+                    href="https://ethereum.org/en/developers/docs/apis/json-rpc/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Ethereum Json Rpc URL{' '}
+                  </Link>
+                  required to load data from chain. Providers like{' '}
+                  <Link href="https://www.infura.io/pricing" target="_blank" rel="noopener noreferrer">
+                    Infura
+                  </Link>{' '}
+                  offer free RPC endpoints.
+                </InfoText>
+              }
+            >
+              <IconButton>
+                <InfoIcon />
+              </IconButton>
+            </Tooltip>
+          </Stack>
+          <Typography>
+            Mythril Parameters
+          </Typography>
+          <StretchedTextfield
             id="execution-timeout"
             label="Execution Timeout"
             type="number"
@@ -84,7 +144,7 @@ export function SettingsDialog(props: DialogProps) {
               });
             }}
           />
-          <TextField
+          <StretchedTextfield
             id="create-timeout"
             label="Create Timeout"
             type="number"
@@ -99,7 +159,7 @@ export function SettingsDialog(props: DialogProps) {
               });
             }}
           />
-          <TextField
+          <StretchedTextfield
             id="max-depth"
             label="Max Depth"
             type="number"
@@ -114,7 +174,7 @@ export function SettingsDialog(props: DialogProps) {
               });
             }}
           />
-          <TextField
+          <StretchedTextfield
             id="solver-timeout"
             label="Solver Timeout"
             type="number"
@@ -145,3 +205,4 @@ export function SettingsDialog(props: DialogProps) {
     </Dialog>
   );
 }
+
